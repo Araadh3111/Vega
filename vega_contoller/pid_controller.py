4893635c26a2b425aa6a73f13c593c59ad1d1191
Araadh <araadh3111@gmail.com>
2026-06-21 23:18:51 +0530

    example commit and contribution will delete later

--- vega_contoller/pid_controller.py
+++ vega_contoller/pid_controller.py
@@ -8,13 +8,13 @@ tiltrate = 1
 target = 0
 error = tiltrate - target
 
-for i in range (100):
+for i in range (10000):
     
     time.sleep(0.005)   
     myturtle.setheading(tiltrate)
     error = tiltrate - target
     
-    proportionality = error*5
+    proportionality = error*2
     spinrate= tiltrate * 0.1  + spinrate - proportionality
     tiltrate= spinrate * 0.1  + tiltrate
     
