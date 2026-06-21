6847bc1d34df49e77bb98d5993d2734df21b3f04
Araadh <araadh3111@gmail.com>
2026-06-22 00:37:48 +0530

    added I variable

--- vega_contoller/tempCodeRunnerFile.py
+++ vega_contoller/tempCodeRunnerFile.py
@@ -7,15 +7,19 @@ tiltrate = 1
 
 target = 0
 error = tiltrate - target
-
-for i in range (100):
+mv1 = 1
+total_accumulated_error = 0 
+for i in range (10000):
     
     time.sleep(0.005)   
     myturtle.setheading(tiltrate)
     error = tiltrate - target
-    
-    proportionality = error*5
-    spinrate= tiltrate * 0.1  + spinrate - proportionality
+    total_accumulated_error +=error  
+    changeinerror = error - mv1
+    D_force = 10 * changeinerror
+    I_force = total_accumulated_error*0.001
+    proportionality = error*2
+    spinrate= tiltrate * 0.1  + spinrate - proportionality - D_force - 0.5 + I_force
     tiltrate= spinrate * 0.1  + tiltrate
     
     
