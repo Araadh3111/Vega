3c520dd5518755b9db1fe29ca5f788f6289e2f60
Araadh <araadh3111@gmail.com>
2026-06-22 13:18:35 +0530

    final flight controller simulator done

--- vega_contoller/pid_controller.py
+++ vega_contoller/pid_controller.py
@@ -7,23 +7,25 @@ tiltrate = 1
 
 target = 0
 error = tiltrate - target
-
-for i in range (100):
+mv1 = 1
+total_accumulated_error = 0 
+for i in range (10000):
     
     time.sleep(0.005)   
     myturtle.setheading(tiltrate)
-    error = tiltrate - target
     
-    proportionality = error*5
-    spinrate= tiltrate * 0.1  + spinrate - proportionality
+    
+    error = tiltrate - target
+    total_accumulated_error +=error  
+    changeinerror = error - mv1
+    D_force = 10 * changeinerror
+    I_force = total_accumulated_error*0.001
+    proportionality = error*2
+    spinrate= tiltrate * 0.1  + spinrate - proportionality - D_force  -I_force - 0.5 
     tiltrate= spinrate * 0.1  + tiltrate
     
     
     print(error)
     mv1 = error
-
-
-
-
 
 screen.exitonclick()
