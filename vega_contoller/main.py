a49df39b657c54d4748926e10f83dbc6fdcd3091
Araadh <araadh3111@gmail.com>
2026-07-21 23:02:37 +0530

    slider added and works

--- vega_contoller/main.py
+++ vega_contoller/main.py
@@ -1,7 +1,7 @@
 #import matplotlib to see the data in graph 
 import matplotlib.pyplot as plt
 #Objective to make a program that calculates errors every second
-
+from matplotlib.widgets import Slider
 
 def simulate(Kp, Kd, Ki):
     setpoint = 0 #the angle I want my rocket to be at
@@ -28,17 +28,36 @@ def simulate(Kp, Kd, Ki):
         current_angle += angular_speed*dt 
         tracker=error
         time+=dt
-        print(f"current angle = {round(current_angle, 2)}")
+        
         current_angle_list.append(current_angle)
         time_list.append(time)
 
     return time_list, current_angle_list
-time_list, current_angle_list = simulate(60, 25, 100)
+time_list, current_angle_list = simulate(200, 25, 100)
 #print(current_angle_list)    
-plt.plot(time_list,current_angle_list,label="Kp = 60")
+line, = plt.plot(time_list, current_angle_list, label="angle")
 plt.legend()
 plt.xlabel("Time (seconds)")
 plt.ylabel("Rocket Angle (degrees)")
 plt.title("Rocket PID stabilization")
 plt.axhline(y=0,color='r',linestyle = '--')
+plt.subplots_adjust(bottom=0.35)
+ax_kp = plt.axes([0.25, 0.20, 0.5, 0.03])
+kp_slider = Slider(ax_kp, "Kp", 0, 500, valinit=200)
+ax_kd = plt.axes([0.25, 0.12, 0.5, 0.03])
+kd_slider = Slider(ax_kd, "Kd", 0, 100, valinit=25)
+ax_ki = plt.axes([0.25, 0.04, 0.5, 0.03])
+ki_slider = Slider(ax_ki, "Ki", 0, 300, valinit=100)
+
+def update(val):
+    # after moving a slider the graph should also update 
+    
+    
+    time_list, current_angle_list = simulate(kp_slider.val, kd_slider.val, ki_slider.val)
+    line.set_ydata(current_angle_list)
+    plt.gcf().canvas.draw_idle()
+
+kp_slider.on_changed(update)   
+kd_slider.on_changed(update)
+ki_slider.on_changed(update)
 plt.show()
--- vega_contoller/notes.md
+++ vega_contoller/notes.md
@@ -6,3 +6,6 @@ going to learn differential term
 7/21/2026
 6:28 pm
 added Integral term, made a graph which I will use for tuning later onwards and wrapped everything in a function called simulate with parameters for Kp, Kd, Ki.
+
+11:02 pm:
+finally made the graph move by sliders! feeling great!
